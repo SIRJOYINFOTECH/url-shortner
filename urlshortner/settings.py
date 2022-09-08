a903c14765608d4813453eadbecddfa64f59634e
SIRJOYINFOTECH <joyobafemi@gmail.com>
2022-09-08 17:06:12 +0100

    A lot of changes

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'urlshortner.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase'
+    }
 }
 
 
